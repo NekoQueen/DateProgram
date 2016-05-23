acdb76fc2927e5059bb81e2482b96090f1beb96f
NekoQueen <celina731@gmail.com>
2016-05-23 17:27:26 -0400

    more stuff

--- Date/src/Answers.java
+++ Date/src/Answers.java
@@ -1,15 +1,16 @@
+import java.util.Random;
+
 
 public class Answers {
-	public static boolean[] male1 = new boolean[5];
-	public static boolean[] male2 = new boolean[5];
-	public static boolean[] male3 = new boolean[5];
-	public static boolean[] male4 = new boolean[5];
 	
-	public static boolean[] setFakeAnswers(){
-		for(int i = 0)
+	static int random;
+	
+	public static boolean[] setFakeAnswers(boolean[] array){
+		Random random = new Random();
+		for (int i = 0; i < array.length; i++) {
+		    array[i] = random.nextBoolean();
+		}
 		
-		
-		return male1;
-		
+		return array;		
 	}
 }
--- Date/src/Main.java
+++ Date/src/Main.java
@@ -10,13 +10,29 @@ import java.util.List;
 
 public class Main{
 	public static boolean[] userAnswers = new boolean[5];
-	
+	public static boolean[] male1 = new boolean[5];
+	public static boolean[] male2 = new boolean[5];
+	public static boolean[] male3 = new boolean[5];
+	public static boolean[] male4 = new boolean[5];
+	public static boolean[] female1 = new boolean[5];
+	public static boolean[] female2 = new boolean[5];
+	public static boolean[] female3 = new boolean[5];
+	public static boolean[] female4 = new boolean[5];
 	
 	ArrayList<String> numbers = new ArrayList<>(Arrays.asList(""));
 
 	
 	public static void main(String[] args) {
 		
+		Answers.setFakeAnswers(male1);
+		Answers.setFakeAnswers(male2);
+		Answers.setFakeAnswers(male3);
+		Answers.setFakeAnswers(male4);
+		Answers.setFakeAnswers(female1);
+		Answers.setFakeAnswers(female2);
+		Answers.setFakeAnswers(female3);
+		Answers.setFakeAnswers(female4);
+			
 		
 		Profile.createUser();
 	}
